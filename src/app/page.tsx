ea0cf0bcddedab36176b5405a93b92881275a20a
elrrrrrrr <elrrrrrrr@gmail.com>
2023-07-10 19:14:24 +0800

    chore: hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Hello cnpmweb.
         </p>
         <div>
           <a
