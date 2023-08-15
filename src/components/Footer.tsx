da762909f0a6937c0c8fdb6794b3e76bccad53f3
elrrrrrrr <elrrrrrrr@gmail.com>
2023-08-15 13:48:50 +0800

    chore: remove cnzz

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,12 +24,6 @@ export default function Footer() {
       >
         浙ICP备15033595号-63
       </Link>
-      <script
-        type='text/javascript'
-        dangerouslySetInnerHTML={{
-          __html: `var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5874717'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/stat.php%3Fid%3D5874717%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));</script><script>(function() { $("body").attr("data-spm", "24755359"); $("head").append("<meta name=\"data-spm\" content=\"a2c6h\">"); })();</script> <script type="text/javascript" nonce="81dd15b10fc44ca4bba7c8b2b17b6f53">  (function  (d)  { var  t=d.createElement("script");t.type="text/javascript";t.async=true;t.id="tb-beacon-aplus";t.setAttribute("exparams","category=&userid=&aplus&yunid=&yunpk=&channel=&cps=");t.src="//g.alicdn.com/alilog/mlog/aplus_v2.js";d.getElementsByTagName("head")[0].appendChild(t);})(document);`,
-        }}
-      ></script>
     </footer>
   );
 }
