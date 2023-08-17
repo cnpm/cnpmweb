ca6fe5faa1595952b1952b59f6b4f76d31d36f74
elrrrrrrr <elrrrrrrr@gmail.com>
2023-08-17 17:22:23 +0800

    chore: deploy

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,6 @@
 
 PROCESS_NAME="cnpmweb/node_modules"
 
-# 判断是否提供了进程名称
-if [ -z "$PROCESS_NAME" ]; then
-    echo "Please provide a process name."
-    exit 1
-fi
-
 # 使用 pgrep 查找包含提供名称的进程
 PIDS=$(pgrep -f "$PROCESS_NAME")
 
@@ -18,3 +12,5 @@ if [ ! -z "$PIDS" ]; then
 else
     echo "No processes found containing '$PROCESS_NAME'."
 fi
+
+nohup npm run start &
