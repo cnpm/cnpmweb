834185742c2610dc6c6c2d91ce09083f14d18ad1
elrrrrrrr <elrrrrrrr@gmail.com>
2023-09-04 09:42:31 +0800

    feat: add search results page (#35)
    > closes https://github.com/cnpm/cnpmweb/issues/27
    * 🧶 添加搜索结果页, `/packages?q&page`，展示部分缩略信息
    * 💄 调整首页输入交互，点击快速搜索结果直接跳转，输入回车则跳转搜索结果页
    * 📚 分页每页设置 12 条，由 es 限制，最多返回 `10000` 条数据
    
    ---------
    
    
    ![image](https://github.com/cnpm/cnpmweb/assets/5574625/d338caff-b793-4fce-b153-0354ceb9da0a)

--- src/components/LandingSearch.tsx
+++ src/components/LandingSearch.tsx
@@ -4,26 +4,26 @@ import React, { useState } from 'react';
 import { Input, Typography, AutoComplete } from 'antd';
 import { useCachedSearch } from '@/hooks/useSearch';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function LandingSearch() {
   const [search, setSearch] = useState('');
   const router = useRouter();
-  const jumpRef = React.useRef(false);
 
   const { data: searchResult, isLoading } = useCachedSearch({
     keyword: search,
-    page: 0,
+    page: 1,
   });
 
   const options = React.useMemo(() => {
-    if (!searchResult) {
+    if (!searchResult?.objects) {
       return [];
     }
 
     return searchResult.objects.map(
       (object) => ({
         label: (
-          <>
+          <Link href={`/package/${object.package.name}`}>
             <Typography.Text>
               {object.package.name}@{object.package.version}
             </Typography.Text>
@@ -31,7 +31,7 @@ export default function LandingSearch() {
             <Typography.Text type='secondary'>
               {object.package.description}
             </Typography.Text>
-          </>
+          </Link>
         ),
         value: object.package.name,
       })
@@ -43,18 +43,14 @@ export default function LandingSearch() {
       style={{ width: '100%' }}
       options={options}
       onChange={setSearch}
-      onSelect={(selectValue) => {
-        const targetUrl = `/package/${selectValue}/home`;
-        if (targetUrl) {
-          jumpRef.current = true;
-          router.push(targetUrl);
-        }
-      }}
     >
       <Input.Search
         size='large'
         placeholder='输入 NPM 包名、作者、关键字等信息即可搜索...'
         enterButton
+        onSearch={() => {
+          router.push(`/packages?q=${search}`)
+        }}
         loading={!!(search && isLoading)}
       />
     </AutoComplete>
