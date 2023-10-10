920ad4a4973861ae17b4cd36c952068cc90a339c
elrrrrrrr <elrrrrrrr@gmail.com>
2023-10-10 06:18:09 -0500

    feat: hire (#43)
    * 更好的世界 更好的你 😇

---
+++ src/components/AdHire.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { Alert, Button } from "antd";
+import Link from "next/link";
+
+export default function AdHire() {
+  return (
+    <Alert
+      banner
+      closable
+      message={
+        <Button href="https://zhuanlan.zhihu.com/p/598748057" target="_blank" type="link">
+          蚂蚁体验技术部-上海招人啦
+        </Button>
+      }
+      showIcon={false}
+      type="info"
+      style={{ textAlign: 'center' }}
+    />
+  );
+}
