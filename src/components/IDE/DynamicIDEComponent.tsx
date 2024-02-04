17b5898072b4cbe08d2e79d5b3fe3a77c3a3f8ae
guqiankun.gqk <guqiankun.gqk@antgroup.com>
2024-02-04 17:38:51 +0800

    chore: add test

--- src/components/IDE/DynamicIDEComponent.tsx
+++ src/components/IDE/DynamicIDEComponent.tsx
@@ -3,8 +3,9 @@ import dynamic from 'next/dynamic';
 export const DynamicIDEComponent = dynamic(
   {
     loader: async () => {
-      console.log('loading IDE');
+      console.log('loading start IDE');
       const IDEModule = await import('./IDE');
+      console.log('loading end IDE');
       return IDEModule.IDE;
     },
     ssr: false,
