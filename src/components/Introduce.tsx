93a5eea9e1fdb0730138764030cc73713194f5f2
fengmk2 <suqian.yf@antgroup.com>
2024-06-27 13:34:34 +0800

    Update Introduce.tsx

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -30,7 +30,7 @@ export default function Introduce() {
         </blockquote>
         目前我们已累积同步了{' '}
         <Text strong>{data?.doc_count ? data?.doc_count.toLocaleString('en-US') : '-'}</Text>{' '}
-        个包，{' '}
+        个包，
         <Text strong>{data?.doc_version_count ? data?.doc_version_count.toLocaleString('en-US') : '-'}</Text>{' '}
         个版本，近7日下载量为{' '}
         <Text strong>
