be40f78e60cc56892f36460f3c45d16191242235
elrrrrrrr <elrrrrrrr@gmail.com>
2025-03-10 00:08:11 +0800

    chore: ad

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function AdBanner() {
   return (
     <Link href="https://render.alipay.com/p/s/taobaonpm_click/image_click_1" target="_blank">
-      <img src="https://render.alipay.com/p/s/taobaonpm_click/image_1" width={'100%'} alt="ad" />
+      <img src="https://mdn.alipayobjects.com/huamei_xmw5ps/afts/img/A*PQdyRb2maiIAAAAAAAAAAAAAet-BAQ/original" width={'100%'} alt="ad" />
     </Link>
   );
 }
