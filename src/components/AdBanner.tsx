384e84997d2438159664c01f4979077819053621
elrrrrrrr <elrrrrrrr@gmail.com>
2025-03-09 20:14:53 -0700

    chore: ad (#99)
    * ⚙ 更新广告位插图
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - The advertisement banner now features a refreshed image that enhances
    the visual appeal of the interface. This update provides a modernized
    look and a cleaner presentation without altering the overall
    functionality. Enjoy the improved visual experience during your regular
    browsing.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function AdBanner() {
   return (
     <Link href="https://render.alipay.com/p/s/taobaonpm_click/image_click_1" target="_blank">
-      <img src="https://render.alipay.com/p/s/taobaonpm_click/image_1" width={'100%'} alt="ad" />
+      <img src="https://u.alipay.cn/_7gtYzHOxywGGcrGkh6szBu" width={'100%'} alt="ad" />
     </Link>
   );
 }
