eba803faa8c7035f096c92637a81fc1fe274ffdf
Dai Wenzhuo <rockdai321@gmail.com>
2025-07-11 14:30:51 +0800

    feat: add DataV advertising (#105)
    @elrrrrrrr 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **New Features**
    * Added an additional advertisement link and image to the AdBanner
    component, displaying multiple ads to users.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -4,8 +4,13 @@ import Link from 'next/link';
 
 export default function AdBanner() {
   return (
+  <>
     <Link href="https://render.alipay.com/p/s/taobaonpm_click/image_click_1" target="_blank">
       <img src="https://u.alipay.cn/_7gtYzHOxywGGcrGkh6szBu" width={'100%'} alt="ad" />
     </Link>
+    <Link href="https://operate.datav.run/advertising/cnpmweb/link" target="_blank">
+      <img src="https://operate.datav.run/advertising/cnpmweb/banner" width={'100%'} alt="ad" />
+    </Link>
+  </>
   );
 }
